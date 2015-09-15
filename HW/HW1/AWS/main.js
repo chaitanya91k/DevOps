a18f79598aeca386a43a3aeb3194b6bb4f184458
Karishma <knimgao@ncsu.edu>
2015-09-15 19:27:35 -0400

    Partial Implementation of Instance creation

---
+++ HW/HW1/AWS/main.js
@@ -0,0 +1,18 @@
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./aws_config.json');
+
+var ec2 = new AWS.EC2();
+
+var params = {
+  ImageId: 'ami-1624987f', // Amazon Linux AMI x86_64 EBS
+  InstanceType: 't1.micro',
+  MinCount: 1, MaxCount: 1
+};
+
+// Create the instance
+ec2.runInstances(params, function(err, data) {
+  if (err) { console.log("Could not create instance", err); return; }
+
+  var instanceId = data.Instances[0].InstanceId;
+  console.log("Created instance", instanceId);
+});
