680903cbe25be6a3a0dd8d3806f330f2d1b10daa
Karishma Nimgaonkar <karunim28@gmail.com>
2015-11-02 20:56:13 -0500

    got 'recent' working

--- HW/HW3/Queues/main.js
+++ HW/HW3/Queues/main.js
@@ -4,8 +4,8 @@ var express = require('express')
 var fs      = require('fs')
 var app = express()
 // REDIS
-//var client = redis.createClient(6379, '127.0.0.1', {})
-
+var client = redis.createClient(6379, '127.0.0.1', {})
+var length;
 ///////////// WEB ROUTES
 
 // Add hook to make it easier to get all visited URLS.
@@ -14,10 +14,19 @@ app.use(function(req, res, next)
 	console.log(req.method, req.url);
 
 	// ... INSERT HERE.
+	client.lpush('queue', req.url, function(err, reply) {
+		console.log("Length: ", reply);
+		client.ltrim('queue', 0, 4);
+		client.lrange('queue',0,-1, function(error, queue) {
+	        console.log("queue: " + queue);
+	    })
+
+	})
+	
+	// console.log("Queue length: ", client.llen('queue'))
 
 	next(); // Passing the request to the next handler in the stack.
 });
-
 
 // app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
 //    console.log(req.body) // form fields
@@ -48,11 +57,28 @@ app.use(function(req, res, next)
 // })
 
 // HTTP SERVER
-// var server = app.listen(3000, function () {
+var server = app.listen(3000, function () {
 
-//   var host = server.address().address
-//   var port = server.address().port
+  var host = server.address().address
+  var port = server.address().port
 
-//   console.log('Example app listening at http://%s:%s', host, port)
-// })
+  console.log('Example app listening at http://%s:%s', host, port)
+})
 
+app.get('/', function(req, res) {
+  res.send('hello world!')
+})
+
+app.get('/get', function(req, res) {
+	client.get('newkey', function(err,value){ res.send(value)});
+})
+
+app.get('/set', function(req, res) {
+	client.set('newkey', 'this message will self-destruct in 10 seconds');
+	client.expire('newkey', 10);
+	res.send('set key with message successfully!')
+})
+
+app.get('/recent', function(req, res) {
+	client.lrange('queue', 0, 4, function(err,value){ res.send(value)});
+})
