58902a12bc83cd7042cc138a1491ad35d9533ec6
Karishma Nimgaonkar <karunim28@gmail.com>
2015-11-21 01:29:47 -0500

    Added docker deploy

---
+++ HW/HW4/DockerDeploy/deploy/green-www/main.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var fs      = require('fs')
+var app = express()
+
+var args = process.argv.slice(2);
+var PORT = args[0];
+
+app.get('/', function(req, res) 
+{
+	res.send("Welcome Green Slice! Docker!");
+});
+
+var server = app.listen(PORT, function () {
+
+	var host = server.address().address
+	var port = server.address().port
+	console.log('Example app listening at http://%s:%s', host, port)
+});
